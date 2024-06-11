0f1ece5227d23848d0ec205ff6efba9760c9ef64
ysb2152 <ysb2152@gmail.com>
2024-06-12 01:50:50 +0900

    Title: 최대공약수 구하기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240612/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240612/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,19 +1,12 @@
 n,m=input().split()
 n=int(n)
 m=int(m)
-k=0
-for i in range(1,101):
-  if i==n or i==m:
-    if n==m:
-        k=n
-    if n==1 or m==1:
-        k=1
-    else:
-        break
-  if i>n or i>m:
-    i=1
-  if (n%i)==0 & (m%i)==0:
-    k=i
-    
 
-print(k)
+def gcd(n,m):
+  k=0
+  for i in range(1, min(n,m)+1):
+   if n%i==0 and m%i==0:
+     k=i
+  print(k)
+
+gcd(n,m)
