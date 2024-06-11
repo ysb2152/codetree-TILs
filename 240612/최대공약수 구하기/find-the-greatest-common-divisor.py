6b9ffb9c1581f9a3080e7917fc973c77635ecf39
ysb2152 <ysb2152@gmail.com>
2024-06-12 01:42:06 +0900

    Title: 최대공약수 구하기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240612/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240612/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -6,8 +6,12 @@ for i in range(1,101):
   if i==n or i==m:
     if n==m:
         i=n
+    if n==1 or m==1:
+        k=1
     else:
         break
+  if i>n or i>m:
+    i=1
   if (n%i)==0 & (m%i)==0:
     k=i
     
