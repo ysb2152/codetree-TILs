0e3f36b286a74d81cdae67463a7fe96cc01d93ca
ysb2152 <ysb2152@gmail.com>
2024-06-27 00:50:57 +0900

    Title: 알파벳에 따른 평가, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240627/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+i=input()
+
+if i=='S':
+    print("Superior")
+elif i=='A':
+    print("Excellent")
+elif i=='B':
+    print("Good")
+elif i=='C':
+    print("Usually")
+elif i=='D':
+    print("Effort")
+else:
+    print("Failure")
