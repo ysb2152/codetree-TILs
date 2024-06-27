6b36b9ed9ea4159d764b7f514fc40f1125bbe699
ysb2152 <ysb2152@gmail.com>
2024-06-27 23:02:48 +0900

    Title: 두 사람, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240627/두 사람/two-person.py
+++ 240627/두 사람/two-person.py
@@ -2,10 +2,7 @@ aa,ae=input().split()
 ba,bs=input().split()
 aa=int(aa)
 ba=int(ba)
-if (aa>=19 or ba>=19):
-    if ae=='M' or bs=='M':
-        print("1")
-    else:
-        print("0")
+if (aa>=19 and ae=='M') or (ba>=19 and bs=='M'):
+    print("1")
 else:
     print("0")
