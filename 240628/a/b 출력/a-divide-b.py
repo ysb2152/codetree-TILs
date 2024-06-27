15537f3a12a404095aa84fa2e11de90f4648b690
ysb2152 <ysb2152@gmail.com>
2024-06-28 04:18:44 +0900

    Title: a/b 출력, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240628/a/b 출력/a-divide-b.py
+++ 240628/a/b 출력/a-divide-b.py
@@ -1,8 +1,11 @@
 a,b=map(int,input().split())
 c=[]
+d=a
+e=b
 for _ in range(21):
     a=(a-(a//b)*b)*10
     c.append(a//b)
-print("0.",end="")
+
+print(f"{d//e}.",end="")
 for i in range(0,20):
     print(f"{c[i]}",end="")
