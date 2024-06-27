00b4de825bb18c23a80da43d3d61bcffea0c98b4
ysb2152 <ysb2152@gmail.com>
2024-06-27 23:00:07 +0900

    Title: 두 사람, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240627/두 사람/two-person.py
+++ 240627/두 사람/two-person.py
@@ -2,7 +2,10 @@ aa,ae=input().split()
 ba,bs=input().split()
 aa=int(aa)
 ba=int(ba)
-if (aa>=19 or ba>=19) and (ae=='M' or bs=='M'):
-    print("1")
+if (aa>=19 or ba>=19):
+    if ae=='M' or bs=='M':
+        print("1")
+    else:
+        print("0")
 else:
     print("0")
