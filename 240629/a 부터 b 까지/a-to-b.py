dd1cc72fb27561e188f3bee9ddbc90c1742fb3f4
ysb2152 <ysb2152@gmail.com>
2024-06-29 00:19:10 +0900

    Title: a 부터 b 까지, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240629/a 부터 b 까지/a-to-b.py
+++ 240629/a 부터 b 까지/a-to-b.py
@@ -1,9 +1,13 @@
 a,b=map(int,input().split())
 print(f"{a}",end=" ")
 while a<b:
- if a%2==1:
+    if a%2==1:
         a*=2
+        if a>b:
+            break
         print(f"{a}",end=" ")    
- if a%2==0:
+    if a%2==0:
         a+=3
+        if a>b:
+            break
         print(f"{a}",end=" ")
