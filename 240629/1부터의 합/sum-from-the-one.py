1bc0077a527c1b13e7188f464d2c02f9f6a199b2
ysb2152 <ysb2152@gmail.com>
2024-06-29 05:57:27 +0900

    Title: 1부터의 합, Time: 215ms, Memory: 25MB, Status: Passed - CodeTree

--- 240629/1부터의 합/sum-from-the-one.py
+++ 240629/1부터의 합/sum-from-the-one.py
@@ -1,11 +1,7 @@
 n=int(input())
 summ=0
 for i in range(1,101):
-    if summ>=n:
-        summ-=(i-1)
-        break
-    if n==1:
-        summ=1
-        break
     summ+=i
-print(f"{summ}")
+    if summ>=n:
+        print(f"{i}")    
+        break
