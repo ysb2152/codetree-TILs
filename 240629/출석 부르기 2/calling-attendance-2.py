a34637f85031a06fbe06b675f6eb55049f0f0608
ysb2152 <ysb2152@gmail.com>
2024-06-29 23:05:16 +0900

    Title: 출석 부르기 2, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240629/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,14 @@
+i=0
+while True:
+    i=int(input())
+    if i==1:
+        print("John")
+    elif i==2:
+        print("Tom")
+    elif i==3:
+        print("Paul")
+    elif i==4:
+        print("Sam")
+    else:
+        print("Vacancy")
+        break
