1d0984f29cfb513caf87c878a9d400b4dbc8df79
ysb2152 <ysb2152@gmail.com>
2024-07-09 00:16:57 +0900

    Title: 1-9 정사각형, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240709/1-9 정사각형/1-9-square.py
@@ -0,0 +1,10 @@
+n=int(input())
+cnt=1
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(f"{cnt}",end="")
+        
+        cnt+=1
+        if cnt>9:
+            cnt=1
+    print(" ")
