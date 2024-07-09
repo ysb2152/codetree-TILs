f9d5d79f9a0a603a4444ad9df40bcca3cee6481f
ysb2152 <ysb2152@gmail.com>
2024-07-09 23:58:48 +0900

    Title: 알파벳 출력하기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240709/알파벳 출력하기/print-alphabet.py
+++ 240709/알파벳 출력하기/print-alphabet.py
@@ -2,6 +2,9 @@ n=int(input())
 cnt=65
 for i in range(1,n+1):
     for j in range(1,i+1):
+        if cnt>90:
+            cnt=65
         print(f"{chr(cnt)}",end="")
         cnt+=1
+        
     print(" ")
