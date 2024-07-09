3f844f647390a8586bda95c8c41814a759643204
ysb2152 <ysb2152@gmail.com>
2024-07-10 02:22:38 +0900

    Title: 배열 채우기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240710/배열 채우기/filling-array.py
+++ 240710/배열 채우기/filling-array.py
@@ -2,8 +2,9 @@ L=list(map(int,input().split()))
 L1=[]
 for ele in L:
     if ele==0:
-        L1=L1[::-1]
         break
+    
     L1.append(ele)
+L1=L1[::-1]
 for ele in L1:
     print(ele,end=" ")
