2634604912ba546abc94af63d6ad0a95bc6577d5
ysb2152 <ysb2152@gmail.com>
2024-07-11 05:03:50 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240711/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240711/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -1,9 +1,11 @@
 n=int(input())
 L=list(map(int,input().split()))
 cnt=0
-for ele in L:
-    if cnt==3:
-        print(f"{L.index(ele)}")
-        break
-    if ele==2:
+
+for i in range(0,n):
+    if L[i]==2:
         cnt+=1
+        
+    if cnt==3:
+        print(f"{i+1}")
+        break
