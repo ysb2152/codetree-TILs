e052fa75e78af1eb013b17db8be2a5f9f6204b2b
ysb2152 <ysb2152@gmail.com>
2024-07-11 01:02:42 +0900

    Title: 십의 자리 숫자, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240711/십의 자리 숫자/number-of-tens-digit.py
@@ -0,0 +1,8 @@
+L=list(map(int,input().split()))
+k=[0 for _ in range(9)]
+for ele in L:
+    if (ele//10)==0:
+        continue
+    k[(ele//10)-1]+=1
+for i in range(0,9):
+    print(f"{i+1} - {k[i]}")
