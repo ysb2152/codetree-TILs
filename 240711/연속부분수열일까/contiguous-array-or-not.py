0c4cd66509723e595993f0dde8bad2b04b172343
ysb2152 <ysb2152@gmail.com>
2024-07-11 04:50:22 +0900

    Title: 연속부분수열일까, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240711/연속부분수열일까/contiguous-array-or-not.py
+++ 240711/연속부분수열일까/contiguous-array-or-not.py
@@ -1,23 +1,23 @@
 n1,n2=map(int,input().split())
 A=list(map(int,input().split()))
 B=list(map(int,input().split()))
-
+C=[]
+D=[]
 for i in range(0,n1):
     for j in range(0,n2):
         if A[i]==B[j]:
             
             C=A[i+1:]
             break
-        else:
-            C=[]
+        
 for i in range(n1-1,-1,-1):
     for j in range(n2-1,-1,-1):
         if A[i]==B[j]:
             
             D=A[:i]
             break
-        else:
-            D=[]
+        
+
 if C==[]:
     print("No")
     exit()
@@ -32,7 +32,7 @@ for ele in C:
 for ele in D:
     if ele in A:
         A.remove(ele)
-
+print(A)
 if A==B:
     print("Yes")
 else:
