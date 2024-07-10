fc0a8f25bc9acb5d9d097ae1b1c2ad99efc2f8a5
ysb2152 <ysb2152@gmail.com>
2024-07-11 06:17:04 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 159ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240711/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240711/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -4,6 +4,9 @@ cnt=0
 k=[]
 k=set(L)
 k=list(k)
+if len(k)==(len(L)/2):
+    print("-1")
+    exit()
 for p in k:
     cnt=0
     for ele in L:
@@ -12,7 +15,11 @@ for p in k:
             cnt+=1
         if cnt>=2:
          
-         k.remove(ele)
+         if ele in k:
+            
+            k.remove(ele)
+            cnt=0
+        
         
 if k==[]:
     print("-1")
