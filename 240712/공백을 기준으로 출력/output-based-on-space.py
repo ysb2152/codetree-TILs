7d32479f97a796b3a1d13b84e8ddb797362e8ead
ysb2152 <ysb2152@gmail.com>
2024-07-12 04:28:59 +0900

    Title: 공백을 기준으로 출력, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240712/공백을 기준으로 출력/output-based-on-space.py
+++ 240712/공백을 기준으로 출력/output-based-on-space.py
@@ -2,12 +2,14 @@ a=input()
 b=input()
 a=list(a)
 b=list(b)
-for ele in a:
-    if ele==' ':
-        a.remove(ele)
-for ele in b:
-    if ele==' ':
-        b.remove(ele)
+for _ in range(len(a)):
+    for ele in a:
+        if ele==' ':
+            a.remove(ele)
+for _ in range(len(b)):
+    for ele in b:
+        if ele==' ':
+            b.remove(ele)
 for ele in a:
     print(ele,end="")
 for ele in b:
