8b0349663dc584eb77388e80570d33af36e23c17
ysb2152 <ysb2152@gmail.com>
2024-07-11 23:29:52 +0900

    Title: 배열의 평균, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240711/배열의 평균/ave-of-array.py
@@ -0,0 +1,18 @@
+L=[list(map(int,input().split())) for _ in range(2)]
+g=0
+s=0
+a=0
+for i in range(0,2):
+    for j in range(0,4):
+        g+=L[i][j]
+        a+=L[i][j]
+    print(f"{g/4}",end=" ")
+    g=0
+print(" ")
+for i in range(0,4):
+    for j in range(0,2):
+        s+=L[j][i]
+    print(f"{s/2}",end=" ")
+    s=0
+print(" ")
+print(f"{a/8}")
