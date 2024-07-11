1ea0193c1c5dcb56d095d9ba728f920c5050c243
ysb2152 <ysb2152@gmail.com>
2024-07-11 23:12:35 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 106ms, Memory: 25MB, Status: Passed - CodeTree

--- 240711/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240711/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -1,5 +1,15 @@
-n=int(input())
-L=list(map(int,input().split()))
-k=[]
-k=L[L.index(min(L)):]
-print(f"{max(k)-min(k)}")
+# 변수 선언 및 입력:
+n = int(input())
+price = list(map(int, input().split()))
+
+# 배열을 앞에서부터 순회하며 사는 시점의 후보를 선택합니다
+max_profit = 0
+for i in range(n):
+    # 사는 시점의 다음 해부터 순회하며 파는 시점의 후보를 선택합니다
+    for j in range(i + 1, n):
+        profit = price[j] - price[i]
+
+        if profit > max_profit:
+            max_profit = profit
+    
+print(max_profit)
