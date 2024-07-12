cb1052a98ec28863e6ae3428707a0ee822daeb9d
ysb2152 <ysb2152@gmail.com>
2024-07-13 02:09:02 +0900

    Title: 특정 문자의 등장 횟수, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240713/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -0,0 +1,21 @@
+c=input()
+b=c
+cnt=0
+cnt1=0
+for i in range(5):
+    if 'ee' in c:
+        cnt+=1
+        
+        c=c[c.find('ee')+1:]
+        
+    else:
+        break
+for i in range(20):
+    if 'eb' in b:
+        cnt1+=1
+        b=b[b.find('eb')+1:]
+        
+    else:
+        break
+print(cnt,end=" ")
+print(cnt1)
