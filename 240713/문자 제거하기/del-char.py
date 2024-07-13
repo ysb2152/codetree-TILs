2b4d84dfa31af59448f3bad64eef48141259adc7
ysb2152 <ysb2152@gmail.com>
2024-07-13 21:32:48 +0900

    Title: 문자 제거하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240713/문자 제거하기/del-char.py
+++ 240713/문자 제거하기/del-char.py
@@ -1,13 +1,13 @@
 c=input()
 
-for _ in range(len(c)-1):
+while len(c)>1:
     k=int(input())
     
     c=list(c)
     if k<len(c):
      c.pop(k)
-
-    if k>len(c):
+    
+    else:
         c.pop(len(c)-1)
 
     c=''.join(c)
