f7838ae47bb0210fed4c977a1adc43ff3f938c13
ysb2152 <ysb2152@gmail.com>
2024-07-14 03:20:01 +0900

    Title: 정수만 추출하기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240714/정수만 추출하기/extract-only-integers.py
+++ 240714/정수만 추출하기/extract-only-integers.py
@@ -2,7 +2,17 @@ a,b=input().split()
 for ele in a:
     if ele.isdigit()==False:
         a=a[:a.find(ele)]
+        break
+
+       
 for ele in b:
+   
     if ele.isdigit()==False:
         b=b[:b.find(ele)]
+        break
+    
+    
+
+
+
 print(int(a)+int(b))
