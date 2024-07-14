9b2c6044727acc4a526f382faa120bff6587ac40
ysb2152 <ysb2152@gmail.com>
2024-07-15 00:35:26 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240715/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240715/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,9 +1,16 @@
 def isT(a,b):
     if b[0] in a:
         k=a[a.index(b[0]):a.index(b[0])+len(b)]
+        
         if k==b:
             return True
         else:
+            c=a[a.index(b[0])+len(b):]
+            if b[0] in c:
+                c=a[a.index(b[0])+len(b):a.index(b[0])+(len(b)*2)]
+            if c==b:
+             return True
+
             return False
 n1,n2=map(int,input().split())
 A=list(map(int,input().split()))
