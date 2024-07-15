3b443507a7d79f9d2b34a2d5963bcd7b53cb946b
ysb2152 <ysb2152@gmail.com>
2024-07-15 23:13:10 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -0,0 +1,25 @@
+L=[]
+chp=0
+def comp(n):
+    global chp
+    if len(L)==1:
+        return L[0]
+    if n==0:
+        
+        return chp
+    if L[n-1]>chp:
+        chp=L[n-1]
+        
+        return comp(n-1)
+    elif L[n-1]<chp:
+        
+        
+        return comp(n-1)
+    elif L[n-1]==L[n-2]:
+        
+        
+        return comp(n-1)
+    
+n=int(input())
+L=list(map(int,input().split()))
+print(comp(n))
