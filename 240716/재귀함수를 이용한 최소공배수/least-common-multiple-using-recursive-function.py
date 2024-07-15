e7663ea81852ddcb116c2b9ca575b09f6d8fe16f
ysb2152 <ysb2152@gmail.com>
2024-07-16 01:19:44 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240716/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,22 @@
+n=int(input())
+L=list(map(int,input().split()))
+G=L[0]
+def GCD(a,b):
+    k=a*b
+    if a>b:
+        while b>0:
+            a,b=b,a%b
+        return k//a
+    if a<b:
+        while a>0:
+            a,b=b%a,a
+        return k//b
+   
+def comp(n):
+    global G
+    if n==0:
+        return G
+    G=GCD(L[n-1],G)
+    
+    return comp(n-1) 
+print(comp(n))
