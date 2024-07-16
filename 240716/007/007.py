7fdb5bf6e99b5d137c69f1557baaee3d07f6bfa0
ysb2152 <ysb2152@gmail.com>
2024-07-16 21:41:06 +0900

    Title: 007, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240716/007/007.py
+++ 240716/007/007.py
@@ -1,4 +1,10 @@
+code,place,time=input().split()
 class no:
     def __init__(A,code,place,time):
-        A.code,A.place,A.time=input().split()
-print(A.code)
+        A.code=code
+        A.place=place
+        A.time=time
+A1=no(code,place,time)
+print(f"secret code : {A1.code}")
+print(f"meeting point : {A1.place}")
+print(f"time : {A1.time} ")
