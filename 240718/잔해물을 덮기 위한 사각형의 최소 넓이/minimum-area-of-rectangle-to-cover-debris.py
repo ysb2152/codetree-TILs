e9b609a6aa755885a928febbfa69752a4e6b3561
ysb2152 <ysb2152@gmail.com>
2024-07-18 02:38:42 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 279ms, Memory: 108MB, Status: Passed - CodeTree

--- 240718/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240718/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -17,7 +17,7 @@ for i in range(2000):
         if L[i][j]==1:
             index.append(i)
             jndex.append(j)
-if 1 not in L:
+if index==[] and jndex==[]:
     print("0")
 else:
     print((max(index)-min(index)+1)*(max(jndex)-min(jndex)+1))
