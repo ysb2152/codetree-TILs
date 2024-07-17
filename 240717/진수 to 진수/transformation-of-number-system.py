8dd9ae15a4772e88022686d68e8b80df64c132c7
ysb2152 <ysb2152@gmail.com>
2024-07-17 20:53:05 +0900

    Title: 진수 to 진수, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/진수 to 진수/transformation-of-number-system.py
+++ 240717/진수 to 진수/transformation-of-number-system.py
@@ -1,16 +1,17 @@
 a,b=map(int,input().split())
 n=input()
 n=list(n)
-L=[]
+
 ans=[]
 num=0
 for i in range(len(n)):
-    num=a*i+int(n[i])
+    num=num*a+int(n[i])
 while True:
     if num<b:
         ans.append(num)
         break
     ans.append(num%b)
     num//=b
+ans=ans[::-1]
 for ele in ans:
     print(ele,end="")
