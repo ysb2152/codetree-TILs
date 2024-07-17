d79cd11bb8ee215280fbe9be0cf856a25e9d2fa1
ysb2152 <ysb2152@gmail.com>
2024-07-17 23:52:53 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240717/흰검 칠하기/painting-white-black.py
+++ 240717/흰검 칠하기/painting-white-black.py
@@ -1,13 +1,18 @@
 n=int(input())
-L=[0 for _ in range(10000)]
-Gray=[0 for _ in range(10000)]
+L=['0' for _ in range(100)]
+Gray=[0 for _ in range(100)]
 curr=0
 point=[]
-offset=5000
+offset=0
 for _ in range(n):
 	x,direction=tuple(input().split())
 	x=int(x)
 	if direction=='L':
+		if x==1:
+			L[curr+offset]='W'
+			Gray[curr+offset]+=1
+			
+			continue	   
 		left=curr-x
 		right=curr
 		curr-=x
@@ -16,24 +21,30 @@ for _ in range(n):
 		for i in range(left,right):
 			L[i]='W'		       
 	else:
+		if x==1:
+			L[curr+offset]+='B'
+			Gray[curr+offset]+=1
+			
+			continue
 		left=curr
 		right=curr+x
 		curr+=x
 		left+=offset
 		right+=offset
-		for i in range(left,right):
+		for i in range(left,right-1):
 			L[i]='B'	
 	point.append([left,right])
-for left,right in point:
-	for i in range(left,right):
-		Gray[i]+=1
+
 Graycnt=0
 Blackcnt=0
 Whitecnt=0
-
+for left,right in point:
+	for i in range(left,right):
+		Gray[i]+=1
 for i in range(len(Gray)):
 	if Gray[i]>=4:
 		L[i]='G'
+
 for ele in L:
 	if ele=='B':
 		Blackcnt+=1
@@ -41,4 +52,5 @@ for ele in L:
 		Graycnt+=1
 	if ele=='W':
 		Whitecnt+=1
+
 print(Whitecnt,Blackcnt,Graycnt)
