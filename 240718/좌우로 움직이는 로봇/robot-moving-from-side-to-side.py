558529a4094d3dcadefe3bd17c312c06c3c45b98
ysb2152 <ysb2152@gmail.com>
2024-07-18 23:21:55 +0900

    Title: 좌우로 움직이는 로봇, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240718/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240718/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -26,24 +26,33 @@ for _ in range(m):
         for i in range(1,t+1):
             B.append(positionB+i)
         positionB+=t
+
 if len(A)>len(B):
     for i in range(1,len(B)):
         if (A[i-1]<A[i]) and (B[i-1]>B[i]) and (A[i]==B[i]):
             cnt+=1
         if (A[i-1]>A[i]) and (B[i-1]<B[i]) and (A[i]==B[i]):
             cnt+=1
-        
+    for j in range(len(B)-1,len(A)):
+        if (B[len(B)-2]<B[len(B)-1]) and  (A[j-1]>A[j]) and (B[len(B)-1]==A[j]):
+            cnt+=1
+        if (B[len(B)-2]>B[len(B)-1]) and  (A[j-1]<A[j]) and (B[len(B)-1]==A[j]):
+            cnt+=1
 if len(A)<len(B):
+    for i in range(1,len(A)):
+        if A[i-1]!=B[i-1] and A[i]==B[i]:
+            cnt+=1
+        
+    for j in range(len(A)-1,len(B)):
+        if (A[len(A)-2]!=B[i-1]) and (A[len(A)-1]==B[j]):
+            cnt+=1
+       
+if len(A)==len(B):
     for i in range(1,len(A)):
         if (A[i-1]<A[i]) and (B[i-1]>B[i]) and (A[i]==B[i]):
             cnt+=1
         if (A[i-1]>A[i]) and (B[i-1]<B[i]) and (A[i]==B[i]):
             cnt+=1
-if len(A)==len(B):
-        if (A[i-1]<A[i]) and (B[i-1]>B[i]) and (A[i]==B[i]):
-            cnt+=1
-        if (A[i-1]>A[i]) and (B[i-1]<B[i]) and (A[i]==B[i]):
-            cnt+=1
-      
+     
 
 print(cnt+1)
