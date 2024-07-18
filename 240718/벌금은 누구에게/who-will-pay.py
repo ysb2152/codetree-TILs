8c2d161c6da6edd86ed5a671e9ed39a0517f4e42
ysb2152 <ysb2152@gmail.com>
2024-07-18 22:02:04 +0900

    Title: 벌금은 누구에게, Time: 133ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240718/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,13 @@
+n,m,k=map(int,input().split())
+L=[0 for _ in range(n)]
+p=[]
+for i in range(m):
+    a=int(input())
+    L[a-1]+=1
+for i in range(n):
+    if L[i]>=k:
+        p.append(i+1)
+if p!=[]:
+    print(p[0])
+else:
+    print("-1")
