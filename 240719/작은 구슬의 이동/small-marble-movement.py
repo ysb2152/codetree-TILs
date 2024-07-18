4fcf2a10dd747a00e91295eb41f237e7c20f335e
ysb2152 <ysb2152@gmail.com>
2024-07-19 06:08:59 +0900

    Title: 작은 구슬의 이동, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/작은 구슬의 이동/small-marble-movement.py
+++ 240719/작은 구슬의 이동/small-marble-movement.py
@@ -1,32 +1,44 @@
 n,t=map(int,input().split())
 r,c,d=input().split()
 r,c=int(r),int(c)
-L=[[0 for _ in range(n)]for _ in range(n)]
+
 def in_range(a,b):
-    return 1<=a and a<n and 1<=b and b<n
+    return 0<a and a<=n and 0<b and b<=n
 dx,dy=[0,1,-1,0],[1,0,0,-1]
 cnt=0
 D={'R':0,'D':1,'U':2,'L':3}
-for i in range(t):
+for i in range(1,t+1):
     if d=='R':
         r,c=r+dx[D[d]],c+dy[D[d]]
-        
         if in_range(r,c)==False:
+            
             r,c=r-dx[D[d]],c-dy[D[d]]
             d='L'
+            continue
+            
+            
     if d=='D':
         r,c=r+dx[D[d]],c+dy[D[d]]
         if in_range(r,c)==False:
             r,c=r-dx[D[d]],c-dy[D[d]]
             d='U'
+            continue
+            
     if d=='U':
         r,c=r+dx[D[d]],c+dy[D[d]]
         if in_range(r,c)==False:
             r,c=r-dx[D[d]],c-dy[D[d]]
             d='D'
+            continue
+            
     if d=='L':
         r,c=r+dx[D[d]],c+dy[D[d]]
+        
         if in_range(r,c)==False:
+            
             r,c=r-dx[D[d]],c-dy[D[d]]
             d='R'
+            continue
+    
+  
 print(r,c)
