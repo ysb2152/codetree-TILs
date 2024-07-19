80063f583b2a722f085cb87f230742b314c21411
ysb2152 <ysb2152@gmail.com>
2024-07-19 20:51:25 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 240719/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
+++ 240719/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -3,7 +3,7 @@ L=[[0 for _ in range(m)]for _ in range(n)]
 dx,dy=[0,1,0,-1],[1,0,-1,0]
 def in_range(a,b):
     return 0<=a and a<n and 0<=b and b<m
-cnt=66
+cnt=65
 dirc=0
 x,y=0,0
 L[0][0]='A'
@@ -12,9 +12,10 @@ for i in range(1,n*m):
     if not in_range(dxc,dyc) or L[dxc][dyc]!=0:
         dirc=(dirc+1)%4
     x,y=x+dx[dirc],y+dy[dirc]
-    L[x][y]=chr(cnt+i-1)
-    if cnt==90:
-        cnt=65
+    cnt+=1
+    L[x][y]=chr(cnt)
+    if chr(cnt)=='Z':
+        cnt=64
     
 for row in L:
     for ele in row:
