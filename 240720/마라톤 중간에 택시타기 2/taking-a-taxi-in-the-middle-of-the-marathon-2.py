0cb1d409dd86081a0d3b5014fcd7628435aac79e
ysb2152 <ysb2152@gmail.com>
2024-07-20 03:50:24 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 177ms, Memory: 25MB, Status: Passed - CodeTree

--- 240720/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240720/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -1,43 +1,27 @@
-n=int(input())
-L=[]
-for _ in range(n):
-    a,b=tuple(input().split())
-    L.append([int(a),int(b)])
-possi=[]
-for i in range(1,n-1):
-    possi.append(L[:i]+L[i+1:])
-dis=0
-min_dis=0
-mini=[]
+import sys
 
-if n==3:
-    dis+=abs(possi[0][0][0]-possi[0][1][0])
-    dis+=abs(possi[0][0][1]-possi[0][1][1])
-    mini.append(dis)
-if n==4:
-    for i in range(len(possi)):
-        for j in range(1,len(possi[i])-1):
-            dis=0
-        
-            dis+=abs(possi[i][0][0]-possi[i][1][0])
-            dis+=abs(possi[i][0][1]-possi[i][1][1])
-                
-            for k in range(0,2):
-                dis+=abs(possi[i][j][k]-possi[i][j+1][k])
-            mini.append(dis)
-        
-    
-else:
-    for i in range(len(possi)):
-        for j in range(1,len(possi[i])-1):
-            dis=0
-        
-            dis+=abs(possi[i][0][0]-possi[i][1][0])
-            dis+=abs(possi[i][0][1]-possi[i][1][1])
-            dis+=abs(possi[i][n-3][0]-possi[i][n-2][0])
-            dis+=abs(possi[i][n-3][1]-possi[i][n-2][1])
-        
-            for k in range(0,2):
-                dis+=abs(possi[i][j][k]-possi[i][j+1][k])
-            mini.append(dis)
-print(min(mini))
+INT_MAX = sys.maxsize
+
+# 변수 선언 및 입력
+n = int(input())
+arr = [
+	list(map(int, input().split()))
+	for _ in range(n)
+]
+
+# 각 i번째 체크포인트를 건너 뛰었을 때의 거리를 구해줍니다.
+ans = INT_MAX
+for i in range(1, n - 1):
+	# 거리를 구합니다.
+	dist = 0
+	prev_idx = 0
+	for j in range(1, n):
+		if j == i:
+			continue
+		dist += abs(arr[prev_idx][0] - arr[j][0]) + abs(arr[prev_idx][1] - arr[j][1])
+		prev_idx = j
+	
+	ans = min(ans, dist)
+
+# 출력
+print(ans)
