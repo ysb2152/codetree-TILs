90912bd9f7ff77a073a58e5167045e08d12c39f3
ysb2152 <ysb2152@gmail.com>
2024-07-20 04:21:55 +0900

    Title: 특정 수와 근접한 합, Time: 116ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240720/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -0,0 +1,18 @@
+n,s=map(int,input().split())
+L=list(map(int,input().split()))
+L1=[]
+L2=[]
+for i in range(len(L)):
+    L1.append(L[:i]+L[i+1:])
+for i in range(len(L1)):
+    for j in range(len(L1[1])):
+        L2.append(L1[i][:j]+L1[i][j+1:])
+
+for i in range(len(L2)):
+    cnt=0
+    for j in range(n-2):
+        cnt+=L2[i][j]
+    L2[i]=cnt
+for i in range(len(L2)):
+    L2[i]=L2[i]-s
+print(min(L2))
