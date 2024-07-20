80f9a5dedf7abab40e738fd8c23f7dcb34507079
ysb2152 <ysb2152@gmail.com>
2024-07-21 01:44:45 +0900

    Title: 아름다운 수열 2, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240721/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,11 @@
+n, m = tuple(map(int, input().split()))
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+# 모든 구간의 시작점을 잡아봅니다.
+cnt = 0
+for i in range(0, n - m + 1):
+	if sorted(arr1[i:i+m]) == sorted(arr2):
+		cnt += 1
+
+print(cnt)
