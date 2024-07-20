d07f9553a3aba26c2e63869d41920e30a2b8860d
ysb2152 <ysb2152@gmail.com>
2024-07-20 21:17:18 +0900

    Title: 씨 오 더블유 2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240720/씨 오 더블유 2/c-o-w-2.py
+++ 240720/씨 오 더블유 2/c-o-w-2.py
@@ -2,11 +2,23 @@ n=int(input())
 L=input()
 L=list(L)
 c,o,w=0,0,0
+cnt=0
 for i in range(n):
+	o=0
+	w=0
 	if L[i]=='C':
-		c+=1
-	if L[i]=='O':
-		o+=1
-	if L[i]=='W':
-		w+=1
-print(c*o*w)
+		for j in range(i+1,n):
+			if L[j]=='O':
+				o+=1
+			if L[j]!='O':
+				continue
+			
+			for k in range(j+1,n):
+				if L[k]=='W':
+					w+=1
+			cnt+=w*o
+			w=0
+			o=0
+			
+	
+print(cnt)
