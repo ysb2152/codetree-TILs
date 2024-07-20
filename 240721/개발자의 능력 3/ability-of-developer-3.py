856efb251f459000f6531032689907fbe04cbb19
ysb2152 <ysb2152@gmail.com>
2024-07-21 04:17:30 +0900

    Title: 개발자의 능력 3, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/개발자의 능력 3/ability-of-developer-3.py
+++ 240721/개발자의 능력 3/ability-of-developer-3.py
@@ -1,9 +1,10 @@
+import sys
 L=list(map(int,input().split()))
 def summ(a,b,c):
     sum1=L[a]+L[b]+L[c]
     sum2=sum(L)-sum1
     return abs(sum1-sum2)
-mini=200
+mini=sys.maxsize
 for i in range(0,6):
     for j in range(i+1,6):
         for k in range(j+1,6):
