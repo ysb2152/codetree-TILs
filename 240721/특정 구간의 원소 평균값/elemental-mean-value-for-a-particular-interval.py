eb05bf0cab08a976371c7a6206587768a28840c9
ysb2152 <ysb2152@gmail.com>
2024-07-21 00:47:46 +0900

    Title: 특정 구간의 원소 평균값, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240721/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240721/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -4,9 +4,19 @@ cnt=0
 for i in range(n):
     for j in range(i,n):
         sum_val=0
+        
         for k in range(i,j+1):
+            if i==j:
+                sum_val+=L[i]*2
+                continue
             sum_val+=L[k]
-        sum_val//=(j+1)
+        if i==j:
+            sum_val/=2
+            cnt+=1
+            continue
+        
+        sum_val/=(j-i+1)
+        
         if sum_val in L[i:j+1]:
             cnt+=1
 print(cnt)
