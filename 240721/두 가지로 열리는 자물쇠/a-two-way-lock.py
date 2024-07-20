8d7708f54ed2554c5eaa93dc65e1d94a876e7c44
ysb2152 <ysb2152@gmail.com>
2024-07-21 05:52:55 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240721/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240721/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -1,25 +1,43 @@
 n=int(input())
 a1,b1,c1=map(int,input().split())
 a2,b2,c2=map(int,input().split())
-nopen=[]
+open1=[]
+o=[[],[],[]]
+p=[[],[],[]]
+cnt=0
+result=[]
 for i in range(1,n+1):
-    if abs(i-a1)<=2 or (i==n and a1+i<=n+2) or (i==n-1 and a1+i==n) or ((i==1 or i==2) and a1==n) or ((i==1) and a1==n-1):
-        continue
-    for j in range(1,n+1):
-        if abs(j-b1)<=2 or (j==n and b1+j<=n+2) or (j==n-1 and b1+j==n) or ((j==1 or j==2) and b1==n) or ((j==1) and b1==n-1):
-            continue
-        for k in range(1,n+1):
-            if abs(k-c1)<=2or (k==n and c1+k<=n+2) or (k==n-1 and c1+k==n) or ((k==1 or k==2) and c1==n) or ((k==1) and c1==n-1):
-                continue
-            nopen.append([i,j,k])
+    if abs(i-a1)<=2 or (i==n and a1<=2) or (i==n-1 and a1==1) or (a1==n and i<=2) or (a1==n-1 and i==1):
+        o[0].append(i)
+for j in range(1,n+1):
+    if abs(j-b1)<=2 or (j==n and b1<=2) or (j==n-1 and b1==1) or (b1==n and j<=2) or (b1==n-1 and j==1):
+        o[1].append(j)
+for k in range(1,n+1):
+    if abs(k-c1)<=2 or (k==n and c1<=2) or (k==n-1 and c1==1) or (c1==n and k<=2) or (c1==n-1 and k==1):
+        o[2].append(k)
+
+
+
 for i in range(1,n+1):
-    if abs(i-a2)<=2 or (i==n and a2+i<=n+2) or (i==n-1 and a2+i==n) or ((i==1 or i==2) and a2==n) or ((i==1) and a2==n-1):
-        continue
-    for j in range(1,n+1):
-        if abs(j-b2)<=2 or (j==n and b2+j<=n+2) or (j==n-1 and b2+j==n) or ((j==1 or j==2) and b2==n) or ((j==1) and b2==n-1):
-            continue
-        for k in range(1,n+1):
-            if abs(k-c2)<=2or (k==n and c2+k<=n+2) or (k==n-1 and c2+k==n) or ((k==1 or k==2) and c2==n) or ((k==1) and c2==n-1):
+    if abs(i-a2)<=2 or (i==n and a2<=2) or (i==n-1 and a2==1) or (a2==n and i<=2) or (a2==n-1 and i==1):
+        p[0].append(i)
+for j in range(1,n+1):
+    if abs(j-b2)<=2 or (j==n and b2<=2) or (j==n-1 and b2==1) or (b2==n and j<=2) or (b2==n-1 and j==1):
+        p[1].append(j)
+for k in range(1,n+1):
+    if abs(k-c2)<=2 or (k==n and c2<=2) or (k==n-1 and c2==1) or (c2==n and k<=2) or (c2==n-1 and k==1):
+        p[2].append(k)
+
+for a in range(len(o[0])):
+    for b in range(len(o[1])):
+        for c in range(len(o[2])):
+            if [o[0][a],o[1][b],o[2][c]] in open1:
                 continue
-            nopen.append([i,j,k])
-print(len(nopen)-1)
+            open1.append([o[0][a],o[1][b],o[2][c]])
+for a in range(len(p[0])):
+    for b in range(len(p[1])):
+        for c in range(len(p[2])):
+            if [p[0][a],p[1][b],p[2][c]] in open1:
+                continue
+            open1.append([p[0][a],p[1][b],p[2][c]])
+print(len(open1))
