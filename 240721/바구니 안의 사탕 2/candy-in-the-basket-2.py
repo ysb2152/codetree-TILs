6a3f8740cb8252ec22c88c269cf4ed797e977acb
ysb2152 <ysb2152@gmail.com>
2024-07-21 02:04:58 +0900

    Title: 바구니 안의 사탕 2, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240721/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240721/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -7,15 +7,18 @@ for _ in range(n):
     candy,point=map(int,input().split())
     if L[point+offset]!=0:
         L[point+offset]+=candy
+        continue
     L[point+offset]=candy
 
 for c in range(len(L)):
+    if c-k<0 or c+k>10000:
+        continue
     cnt=0
-    L1=L[c-k:c+k+1]
+    L1=L[c-k+1:c+k+2]
     for ele in L1:
-        if ele!=0:
-            cnt+=ele
+        cnt+=ele
     
     
     maxi=max(maxi,cnt)
+
 print(maxi)
