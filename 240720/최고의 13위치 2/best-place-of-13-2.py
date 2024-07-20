a5c38d30cfbd554b111de989886d862f28cee0a2
ysb2152 <ysb2152@gmail.com>
2024-07-20 21:38:33 +0900

    Title: 최고의 13위치 2, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240720/최고의 13위치 2/best-place-of-13-2.py
+++ 240720/최고의 13위치 2/best-place-of-13-2.py
@@ -4,8 +4,12 @@ cnt=0
 for i in range(n):
 	for j in range(n-2):
 		for k in range(n):
-			if k==i:
+			if k==i and n<6:
 				continue
+
+			
 			for l in range(n-2):
+				if k==i and j>n-6:
+					continue
 				cnt=max(cnt,L[i][j]+L[i][j+1]+L[i][j+2]+L[k][l]+L[k][l+1]+L[k][l+2])
 print(cnt)
