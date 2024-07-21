88fea94c8188988fbb7ab87b94dbf18a8eba35f6
ysb2152 <ysb2152@gmail.com>
2024-07-21 23:19:22 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240721/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 240721/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -1,19 +1,12 @@
+import sys
 n=int(input())
 L=[tuple(map(int,input().split()))for _ in range(n)]
-distance=10000
-for i in range(n):
-    
-    for j in range(i+1,n):
-        x1,y1=L[i]
-        x2,y2=L[j]
-        
-    distance=min(distance,((x1-x2)**2+(y1-y2)**2)**0.5)
-
+distance=sys.maxsize
+def dist(i, j):
+    x1, y1 = L[i]
+    x2, y2 = L[j]
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
 for i in range(n):
     for j in range(i+1,n):
-        x1,y1=L[i]
-        x2,y2=L[j]
-    dis=((x1-x2)**2+(y1-y2)**2)**0.5
-    if dis==distance:
-        
-        print((x1-x2)**2+(y1-y2)**2)
+        distance=min(distance,dist(i,j))
+print(distance)
