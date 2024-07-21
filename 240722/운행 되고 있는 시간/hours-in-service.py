983f34918329941e55b6e208de638a95b8e7ef8a
ysb2152 <ysb2152@gmail.com>
2024-07-22 01:41:45 +0900

    Title: 운행 되고 있는 시간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240722/운행 되고 있는 시간/hours-in-service.py
@@ -0,0 +1,22 @@
+n=int(input())
+L=[tuple(map(int,input().split()))for _ in range(n)]
+
+max_time=-999
+for i in range(n):
+    cnt=0
+    time=[0 for _ in range(24)]
+    for j in range(n):
+        if j==i:
+            continue
+        start,end=L[j]
+        
+        for k in range(start,end):
+            time[k]=1
+        
+    for ele in time:
+        if ele==1:
+            cnt+=1
+    
+    max_time=max(max_time,cnt)
+    
+print(max_time)
