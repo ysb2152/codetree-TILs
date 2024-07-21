f59fc00dc2208ffc78ac9ee3450fb3c6cc967220
ysb2152 <ysb2152@gmail.com>
2024-07-22 04:54:25 +0900

    Title: 이상한 폭탄 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240722/이상한 폭탄 2/strange-bomb-2.py
+++ 240722/이상한 폭탄 2/strange-bomb-2.py
@@ -6,7 +6,7 @@ for j in range(0,n-3):
     for k in range(j+1,j+4):
         
         if L[j]==L[k]:
-            EXP.append([L[j]])
+            EXP.append(L[j])
 if EXP==[]:
     print("-1")
 else:
