956f339b98dff18f07aac80c922d24b161afb992
ysb2152 <ysb2152@gmail.com>
2024-07-22 05:10:24 +0900

    Title: 이상한 폭탄 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240722/이상한 폭탄 2/strange-bomb-2.py
+++ 240722/이상한 폭탄 2/strange-bomb-2.py
@@ -2,13 +2,14 @@ n,k=map(int,input().split())
 L=[int(input()) for _ in range(n)]
 EXP=[]
 
-for j in range(n-k+1):
+for j in range(n):
     for k in range(j+1,j+k+1):
         if k>n-1:
             continue
         
         if L[j]==L[k]:
             EXP.append(L[j])
+
 if EXP==[]:
     print("-1")
 else:
