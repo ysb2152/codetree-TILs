1f966146061a9a0acdb2cd6516b010de965246d8
ysb2152 <ysb2152@gmail.com>
2024-07-22 03:35:53 +0900

    Title: 스승의 은혜 3, Time: 422ms, Memory: 73MB, Status: Passed - CodeTree

--- 240722/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240722/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,34 +1,32 @@
-n,b=map(int,input().split())
-L=[tuple(map(int,input().split()))for _ in range(n)]
+# 입력
+import sys
+input = sys.stdin.readline
 
-max_num=0
+n, b = map(int,input().split())
+gift_list = [list(map(int,input().split())) for _ in range(n)]
 
-L.sort(key=lambda x: (x[0]+x[1], x[1]))
-
+# 계산1: 완전 탐색(쿠폰 학생)
+max_res = -1
 for i in range(n):
-    cnt=0
-    num=0
-    for j in range(n):
-        
-        if j==i:
-            cnt+=(L[j][0]//2)+L[j][1]
-            
-            if cnt>b:
-                continue
-            
-            num+=1
-            continue
-        cnt+=L[j][0]+L[j][1]
-        
-        
-        if cnt>b:
-            
-            continue
-        num+=1
-    
-    
 
+    # 1: 학생 선택
+    price, fee = gift_list[i]
+
+    # 2: 학생 적용
+    temp_list = [gift_list[i][:] for i in range(n)]
+    temp_list[i][0] //= 2
+
+    # 3: 가격 정렬
+    temp_list.sort(key=lambda x:(x[0]+x[1]))
     
-    max_num=max(max_num,num)
+    # 4. 최대 학생 수 할당
+    sum_res = sum(temp_list[0]) if b >= sum(temp_list[0]) else 0
+    j = 0 if sum_res > 0 else -1
+    while j + 1 < n and sum_res + sum(temp_list[j + 1]) <= b:
+        sum_res += sum(temp_list[j + 1])
+        j += 1
     
-print(max_num)
+    max_res = max(max_res, j + 1)
+
+# 출력
+print(max_res)
