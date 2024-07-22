701076f94f6c118f2dfa18c2e38ac177106a3fdc
ysb2152 <ysb2152@gmail.com>
2024-07-23 02:06:14 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
+++ 240723/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -2,9 +2,9 @@ A,B,C=map(int,input().split())
 max_compA=C//A
 max_compB=C//B
 max_makeC=0
-for i in range(max_compA):
+for i in range(max_compA+1):
     mul=0
-    for j in range(max_compB):
+    for j in range(max_compB+1):
         mul=i*A+j*B
         if mul>C:
             continue
