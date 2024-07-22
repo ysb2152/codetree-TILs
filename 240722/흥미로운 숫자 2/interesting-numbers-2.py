1027829736bb5c9770458099e12a0cc8d757e816
ysb2152 <ysb2152@gmail.com>
2024-07-22 22:38:13 +0900

    Title: 흥미로운 숫자 2, Time: 262ms, Memory: 25MB, Status: Passed - CodeTree

--- 240722/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240722/흥미로운 숫자 2/interesting-numbers-2.py
@@ -7,10 +7,16 @@ def inter(a):
         if ele not in p:
             p.append(ele)
     if len(p)==2:
-        return 1
+        acount=a.count(p[0])
+        bcount=a.count(p[1])
+        if acount==1 or bcount==1:
+            return 1
+        else:
+            return 0
     else:
         return 0
 for i in range(x,y+1):
     if inter(i)==1:
+        
         cnt+=1
 print(cnt)
