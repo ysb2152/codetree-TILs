877335b89a3ec9aef0ed96efbfa1a621605e0a87
ysb2152 <ysb2152@gmail.com>
2024-07-23 01:41:11 +0900

    Title: 야바위, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/야바위/ya-rock.py
@@ -0,0 +1,48 @@
+n=int(input())
+a1=[1,0,0]
+a2=[0,1,0]
+a3=[0,0,1]
+cnt1=0
+cnt2=0
+cnt3=0
+L=[]
+for i in range(n):
+    a,b,c=list(map(int,input().split()))
+    L.append([a,b,c])
+
+for j in range(n):
+    
+    p,q,r=L[j][0],L[j][1],L[j][2]
+    
+    p-=1
+    q-=1
+    r-=1
+    
+    pr=a1[p]
+    a1[p]=a1[q]
+    a1[q]=pr
+    
+    if a1[r]==1:
+        cnt1+=1
+
+for k in range(n):
+    p,q,r=L[j][0],L[j][1],L[j][2]
+    p-=1
+    q-=1
+    r-=1
+    pr=a2[p]
+    a2[p]=a2[q]
+    a2[q]=pr
+    if a2[r]==1:
+        cnt2+=1
+for l in range(n):
+    p,q,r=L[j][0],L[j][1],L[j][2]
+    p-=1
+    q-=1
+    r-=1
+    pr=a3[p]
+    a3[p]=a3[q]
+    a3[q]=pr
+    if a3[r]==1:
+        cnt3+=1
+print(max(cnt1,cnt2,cnt3))
