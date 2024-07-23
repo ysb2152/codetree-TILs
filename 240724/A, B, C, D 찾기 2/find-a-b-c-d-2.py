3b1fa438dc410e38fdfe70c75bae737ef7156b23
ysb2152 <ysb2152@gmail.com>
2024-07-24 05:21:31 +0900

    Title: A, B, C, D 찾기 2, Time: 211ms, Memory: 26MB, Status: Passed - CodeTree

--- 240724/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 240724/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -1,14 +1,17 @@
 L=list(map(int,input().split()))
 L.sort()
-summ=sum(L)
 
-for A in range(1,max(L)+1):
-    for B in range(1,max(L)+1):
-        for C in range(1,max(L)+1):
-            for D in range(1,max(L)+1):
-                if (A in L) and (B in L) and (C in L) and (D in L):
-                    if (A+B in L) and (B+C in L) and (C+D in L) and (D+A in L) and (A+C in L) and (B+D in L):
-                        if (A+B+C in L) and (A+B+D in L) and (A+C+D in L) and (B+C+D in L):
-                            if (A+B+C+D) in L:
-                                if A<=B and B<=C and C<=D:
-                                    print(A,B,C,D)
+
+for A in range(1,max(L)):
+    
+    for B in range(1,max(L)):
+        
+        for C in range(1,max(L)):
+            
+            for D in range(1,max(L)):
+                
+                if (A+B in L) and (B+C in L) and (C+D in L) and (D+A in L) and (A+C in L) and (B+D in L) and (A+B+C in L) and (A+B+D in L) and (A+C+D in L) and (B+C+D in L) and (A+B+C+D) in L:
+                    if A<=B and B<=C and C<=D:
+                        T=[A,B,C,D]
+                        if (T.count(A)<=L.count(A)) and (T.count(B)<=L.count(B)) and (T.count(C)<=L.count(C)) and (T.count(D)<=L.count(D)):
+                            print(A,B,C,D)
