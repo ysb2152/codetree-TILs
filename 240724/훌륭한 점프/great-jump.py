9d3a8d371bb71bd30bdbca65324c385021c853e8
ysb2152 <ysb2152@gmail.com>
2024-07-24 03:41:12 +0900

    Title: 훌륭한 점프, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/훌륭한 점프/great-jump.py
+++ 240724/훌륭한 점프/great-jump.py
@@ -1,28 +1,30 @@
-n,k=map(int,input().split())
-arr=list(map(int,input().split()))
+MAX_NUM = 100
 
-def is_possible(max_val,k):
-    available_indices = []
-    for i, elem in enumerate(arr):
-        if elem <= max_val:
-            available_indices.append(i)
-    
+# 변수 선언 및 입력:
+n, k = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+
+def is_possible(limit):
+    # 마지막 index로 부터
+    # 숫자 limit을 넘지 않으면서
+    # 거리 k이내로 계속 이동이 가능한지를 판단합니다.
+    last_idx = 0
+    for i in range(1, n):
+        if arr[i] <= limit:
+            if i - last_idx > k:
+                return False
+            last_idx = i
         
-
-    arr_size = len(available_indices)
-    
-   
-    for i in range(1, arr_size):
-        dist = available_indices[i] - available_indices[i - 1]
-       
-        if dist > k:
-            return False
-    
-    
     return True
-minmax = 999999
-for a in range(max(arr),1,-1):
-    if is_possible(a,k):
-        minmax = min(minmax, a)
 
-print(minmax)
+
+# 밟으며 지나간 최댓값을 i라고 가정했을 때
+# 거리 k 이내로 점프하며 끝까지 도달하는 것이
+# 가능한지를 살펴봅니다.
+# 가능하다면, 그때의 i가 최솟값이므로
+# 답을 출력하고 종료합니다.
+for i in range(max(arr[0], arr[n - 1]), MAX_NUM + 1):
+    if is_possible(i):
+        print(i)
+        break
