9532fef2b72261fcf032022806a155b33f439640
ysb2152 <ysb2152@gmail.com>
2024-07-26 21:42:19 +0900

    Title: 독서실의 거리두기 3, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240726/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,25 @@
+n=int(input())
+L=input()
+L=list(L)
+comp=[]
+for i in range(n):
+    cnt=0
+    
+    for j in range(i+1,n):
+        if L[i]=='1'and L[j]=='1':
+            comp.append([i,j,j-i])
+            break
+
+comp.sort(key=lambda x:x[2])
+if comp[len(comp)-1][2]%2==0:
+    L[comp[len(comp)-1][0]+(comp[len(comp)-1][2]//2)]='1'
+min_cnt=9999
+for i in range(n):
+    cnt=0
+    
+    for j in range(i+1,n):
+        if L[i]=='1'and L[j]=='1':
+            cnt=j-i
+            min_cnt=min(min_cnt,cnt)
+            break
+print(min_cnt)
