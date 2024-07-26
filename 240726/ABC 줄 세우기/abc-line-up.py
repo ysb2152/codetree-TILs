6e83fac6c7fecaa820a9808d7a82e261b2190c33
ysb2152 <ysb2152@gmail.com>
2024-07-26 23:29:26 +0900

    Title: ABC 줄 세우기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240726/ABC 줄 세우기/abc-line-up.py
+++ 240726/ABC 줄 세우기/abc-line-up.py
@@ -1,10 +1,12 @@
 n=int(input())
 L=list(input().split())
 cnt=0
-for i in range(n-1):
-    if ord(L[i])>=ord(L[i+1]):
-        a=L[i+1]
-        L[i+1]=L[i]
-        L[i]=a
-        cnt+=1
+for _ in range(n):
+    for i in range(n-1):
+        if ord(L[i])>ord(L[i+1]):
+            a=L[i+1]
+            L[i+1]=L[i]
+            L[i]=a
+            cnt+=1
+
 print(cnt)
