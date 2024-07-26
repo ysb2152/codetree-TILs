4526aa70baad80dbd780b772cd49633739dd3b5a
ysb2152 <ysb2152@gmail.com>
2024-07-27 00:48:53 +0900

    Title: 순위 경쟁2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240727/순위 경쟁2/ranking-competition2.py
+++ 240727/순위 경쟁2/ranking-competition2.py
@@ -2,13 +2,15 @@ n=int(input())
 L=[list(input().split())for _ in range(n)]
 A=0
 B=0
-fame=[]
+fame=['A','B']
 cnt=0
 for i in range(n):
     L[i][1]=int(L[i][1])
 
 for i in range(n):
+    
     beforefame=fame
+    
     if L[i][0]=='A':
         A+=L[i][1]
         if A>B:
