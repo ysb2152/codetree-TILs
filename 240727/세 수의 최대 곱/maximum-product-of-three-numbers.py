9fd1920e937871109c44e036612b19ae7411e7c7
ysb2152 <ysb2152@gmail.com>
2024-07-27 02:18:25 +0900

    Title: 세 수의 최대 곱, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240727/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240727/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -2,36 +2,7 @@ import sys
 n=int(input())
 L=list(map(int,input().split()))
 max_cnt=-sys.maxsize
-
-for i in range(n):
-    cnt=0
-    for j in range(i+1,n):
-        for k in range(j+1,n):
-            if L[i]>0 and L[j]>0 and L[k]>0:
-                cnt=L[i]*L[j]*L[k]
-                max_cnt=max(max_cnt,cnt)
-            if L[i]*L[j]>0 and L[k]>0:
-                cnt=L[i]*L[j]*L[k]
-                max_cnt=max(max_cnt,cnt)
-            if L[i]*L[j]<0 and L[k]<0:
-                cnt=L[i]*L[j]*L[k]
-                max_cnt=max(max_cnt,cnt)
-            if L[i]*L[k]>0 and L[j]>0:
-                cnt=L[i]*L[j]*L[k]
-                max_cnt=max(max_cnt,cnt)
-            if L[i]*L[k]<0 and L[j]<0:
-                cnt=L[i]*L[j]*L[k]
-                max_cnt=max(max_cnt,cnt)
-            if L[j]*L[k]>0 and L[i]>0:
-                cnt=L[i]*L[j]*L[k]
-                max_cnt=max(max_cnt,cnt)
-            if L[j]*L[k]<0 and L[i]<0:
-                cnt=L[i]*L[j]*L[k]
-                max_cnt=max(max_cnt,cnt)
-            if L[i]<0 and L[j]<0 and L[k]<0:
-                cnt=L[i]*L[j]*L[k]
-                max_cnt=max(max_cnt,cnt)
-            if L[i]==0 or L[j]==0 or L[k]==0:
-                cnt=0
-                max_cnt=max(max_cnt,cnt)
-print(max_cnt)
+L.sort()
+max1=L[-1]*L[-2]*L[-3]
+max2=L[0]*L[1]*L[2]
+print(max(max1,max2))
