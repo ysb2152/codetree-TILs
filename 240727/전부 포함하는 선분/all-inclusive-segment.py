45595714427506a34acdaf0eeb1b3989b644e5b4
ysb2152 <ysb2152@gmail.com>
2024-07-27 06:10:19 +0900

    Title: 전부 포함하는 선분, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240727/전부 포함하는 선분/all-inclusive-segment.py
+++ 240727/전부 포함하는 선분/all-inclusive-segment.py
@@ -2,15 +2,21 @@ n=int(input())
 L=[tuple(map(int,input().split()))for _ in range(n)]
 L.sort()
 
+
 t=[]
+s=[]
+t.sort(key=lambda x: x[1])
+for i in range(n):
+    s.append(L[i])
 for i in range(n):
     t.append(L[i])
-t.pop(0)
+t.pop(n-1)
 L.pop(n-1)
 xlayer=[]
 ylayer=[]
 x1layer=[]
 y1layer=[]
+
 for i in range(n-1):
     xlayer.append(L[i][0])
     ylayer.append(L[i][1])
