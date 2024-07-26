e38607d75a4174169b24d1213c9c85ad1e75d8f5
ysb2152 <ysb2152@gmail.com>
2024-07-27 04:21:01 +0900

    Title: 2개씩 그룹짓기 2, Time: 113ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240727/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 240727/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -2,11 +2,10 @@ n=int(input())
 L=list(map(int,input().split()))
 L.sort()
 min_diff=9999
-
-for i in range(1,n):
+L=[0]+L
+for i in range(1,n+1):
         
-    cnt=abs(L[i]-L[i+n])
     
-    min_diff=min(min_diff,cnt) 
+    min_diff=min(min_diff,L[i+n]-L[i]) 
 
 print(min_diff)
