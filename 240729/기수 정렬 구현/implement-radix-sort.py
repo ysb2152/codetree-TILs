a85c84516c99c700e2d980962d5c80ba7dd19e8c
ysb2152 <ysb2152@gmail.com>
2024-07-29 01:39:59 +0900

    Title: 기수 정렬 구현, Time: 180ms, Memory: 35MB, Status: Passed - CodeTree

--- 240729/기수 정렬 구현/implement-radix-sort.py
+++ 240729/기수 정렬 구현/implement-radix-sort.py
@@ -1,27 +1,30 @@
-n=int(input())
-L=list(map(int,input().split()))
-leng=len(str(max(L)))
-for k in range(leng):
-    arr_new=[[] for _ in range(10)]
-    for i in range(len(L)):
-        L[i]=str(L[i])
-        p=len(L[i])-1
+MAX_K = 6
+MAX_DIGIT = 10
+
+# 변수 선언 및 입력
+n = int(input())
+arr = list(map(int, input().split()))
+
+
+def radix_sort():
+    global arr
+
+    p = 1
+    for pos in range(MAX_K):
+        arr_new = [[] for _ in range(MAX_DIGIT)]
+        for elem in arr:
+            digit = (elem // p) % 10
+            arr_new[digit].append(elem)
+
+        arr = []
+        for digit in range(MAX_DIGIT):
+            for elem in arr_new[digit]:
+                arr.append(elem)
         
-        if p<k:
-            digit=0
-        else:
-            L[i]=str(L[i])
-            digit=L[i][k]
-        
-        digit=int(digit)
-        
-        L[i]=int(L[i])
-        arr_new[digit].append(L[i])
-    store=[]
-    for i in range(10):
-        for j in range(len(arr_new[i])):
-            store.append(arr_new[i][j])
-    
-    L=store
-for ele in L:
-    print(ele,end=" ")
+        p *= 10
+
+
+radix_sort()
+
+for elem in arr:
+    print(elem, end=" ")
