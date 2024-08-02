c2b0c102c58758f1689799fed4bf3f68f6b26544
ysb2152 <ysb2152@gmail.com>
2024-08-03 02:35:33 +0900

    Title: 트로미노, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240803/트로미노/tromino.py
@@ -0,0 +1,34 @@
+n,m=map(int,input().split())
+L=[list(map(int,input().split()))for _ in range(n)]
+max_sum=-99999
+for i in range(n):
+    for j in range(m-2):
+        max_sum=max(max_sum,sum(L[i][j:j+3]))
+ReversedL=[]
+for i in range(m):
+    c=[]
+    for j in range(n):
+        c.append(L[j][i])
+    ReversedL.append(c)
+for i in range(n):
+    for j in range(m-2):
+        max_sum=max(max_sum,sum(ReversedL[i][j:j+3]))
+for i in range(n):
+    if i==n-1:
+        continue
+    for j in range(m-1):
+        cnt1=L[i][j]+L[i][j+1]+L[i+1][j]
+        cnt2=L[i][j]+L[i][j+1]+L[i+1][j+1]
+        max_sum=max(max_sum,cnt1)
+        max_sum=max(max_sum,cnt2)
+
+
+for i in range(m):
+    if i==m-1:
+        continue
+    for j in range(n-1):
+        cnt1=ReversedL[i][j]+ReversedL[i][j+1]+ReversedL[i+1][j]
+        cnt2=ReversedL[i][j]+ReversedL[i][j+1]+ReversedL[i+1][j+1]
+        max_sum=max(max_sum,cnt1)
+        max_sum=max(max_sum,cnt2)
+print(max_sum)
