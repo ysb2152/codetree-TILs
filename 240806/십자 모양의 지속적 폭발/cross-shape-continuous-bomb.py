1ea94ae2ec70b4f60f8c4c7eecdbb7414c1a468f
ysb2152 <ysb2152@gmail.com>
2024-08-06 03:28:46 +0900

    Title: 십자 모양의 지속적 폭발, Time: 169ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240806/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.py
+++ 240806/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.py
@@ -6,30 +6,28 @@ dxs,dys=[1,0,-1,0],[0,1,0,-1]
 def in_range(a,b):
     return 0<=a<n and 0<=b<n
 def exp(c):
-    if L==[0]:
-        return 0
+    
     for a in range(n):
         if L[a][c]==0:
             continue
             
         else:
             bomb=L[a][c]
+            
             L[a][c]=0
-    
-        for dx,dy in zip(dxs,dys):
-            for i in range(1,bomb):
-                if in_range(a+i*dx,c+i*dy):
-                    L[a+i*dx][c+i*dy]=0
-        #for row in L:
-            #print(row)
-        #print(" ")
+            for dx,dy in zip(dxs,dys):
+                for i in range(1,bomb):
+                    if in_range(a+i*dx,c+i*dy):
+                        L[a+i*dx][c+i*dy]=0
+            break
+        
 
-ans2=[[0 for _ in range(n)]for _ in range(n)]
-for i in range(m):
-   
-    exp(c[i])
-    
 
+for k in range(m):
+    ans2=[[0 for _ in range(n)]for _ in range(n)]
+    exp(c[k])
+    
+    
     for i in range(n):
         ans2_row=n-1
         for j in range(n-1,-1,-1):
@@ -37,6 +35,10 @@ for i in range(m):
                 ans2[ans2_row][i]=L[j][i]
             
                 ans2_row-=1
+            else:
+                ans2[ans2_row][i]=0
+    
+    
 
 
 for row in ans2:
