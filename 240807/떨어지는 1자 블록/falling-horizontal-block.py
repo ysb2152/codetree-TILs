be27568e6309e5c0b392fdfe9117e0c216f54059
ysb2152 <ysb2152@gmail.com>
2024-08-07 02:12:54 +0900

    Title: 떨어지는 1자 블록, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240807/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240807/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,21 +1,40 @@
-n,m,k=map(int,input().split())
-L=[list(map(int,input().split()))for _ in range(n)]
-flag=False
+# 변수 선언 및 입력: 
+
+n, m, k = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+# 해당 row에 [col_s, col_e] 열에
+# 전부 블럭이 없는지를 확인합니다.
+def all_blank(row, col_s, col_e):
+    return all([
+        not grid[row][col]
+        for col in range(col_s, col_e + 1)
+    ])
+
+
+# 최종적으로 도달하게 될 위치는
+# 그 다음 위치에 최초로 블럭이 존재하는 순간임을 이용합니다.
+def get_target_row():
+    for row in range(n - 1):
+        if not all_blank(row + 1, k, k + m - 1):
+            return row
+
+    return n - 1
+        
+
+k -= 1
+
+# 최종적으로 멈추게 될 위치를 구합니다.
+target_row = get_target_row()
+
+# 최종 위치에 전부 블럭을 표시합니다.
+for col in range(k, k + m):
+    grid[target_row][col] = 1
 
 for i in range(n):
-    for j in range(k-1,k-1+m):
-        if L[i][j]==1:
-            for p in range(k-1,k-1+m):
-                L[i-1][p]=1
-            flag=True
-            break
-    if flag:
-        break
-if not flag:
-    for i in range(n):
-        for j in range(k-1,k-1+m):
-            L[i][j]=1
-for row in L:
-    for ele in row:
-        print(ele,end=" ")
-    print(" ")
+    for j in range(n):
+        print(grid[i][j], end=" ")
+    print()
