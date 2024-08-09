906e1f649fe4600278bcfed42c9c8643112653bb
ysb2152 <ysb2152@gmail.com>
2024-08-10 04:04:45 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 284ms, Memory: 29MB, Status: Passed - CodeTree

--- 240810/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 240810/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -8,10 +8,11 @@ def choose(curr_num):
     if curr_num==n+1:
         print_ans()
         return
-    for i in range(1,n+1):
+    for i in range(1,k+1):
         ans.append(i)
         choose(curr_num+1)
         ans.pop()
+        
         
     return
 choose(1)
