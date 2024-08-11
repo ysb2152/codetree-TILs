51e8294c4fb9e285cecf3dbd38a38d7b9d08bddf
ysb2152 <ysb2152@gmail.com>
2024-08-11 23:48:06 +0900

    Title: 1차원 윷놀이, Time: 859ms, Memory: 29MB, Status: Passed - CodeTree

--- 240811/1차원 윷놀이/yutnori-1d.py
+++ 240811/1차원 윷놀이/yutnori-1d.py
@@ -3,29 +3,26 @@ turn=list(map(int,input().split()))
 area=[i for i in range(1,m+1)]
 max_ap=-99999
 #명령을 수행할 말
-horse=[]
-def move():
+horse=[1 for _ in range(k)]
+def count():
     global max_ap
     cnt=0
-    #모두 1에서 시작
-    start=[1 for _ in range(k)]
-    for i in range(n):
-        for j in range(turn[i]):
-            if start[horse[i]]==m:
-                continue
-            start[horse[i]]+=1
-    max_ap=max(max_ap,start.count(m))
-    #print(start)
+    for i in range(k):
+        if horse[i]>=m:
+            cnt+=1
+    max_ap=max(max_ap,cnt)
+    
+    
         
 def choose(num):
-    if num==n+1:
-        #print(horse)
-        move()
+    if num==n:
+        count()
         return
     for i in range(k):
-        horse.append(i)
+        horse[i]+=turn[num]
         choose(num+1)
-        horse.pop()
+        horse[i]-=turn[num]
+        
     return
-choose(1)
+choose(0)
 print(max_ap)
