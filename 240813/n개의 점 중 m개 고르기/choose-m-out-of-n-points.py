0a0a614115927cc88b6c96fa0b7997367642c19c
ysb2152 <ysb2152@gmail.com>
2024-08-13 03:01:33 +0900

    Title: n개의 점 중 m개 고르기, Time: 268ms, Memory: 26MB, Status: Passed - CodeTree

--- 240813/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240813/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -1,37 +1,46 @@
 import sys
 n,m=map(int,input().split())
 L=[tuple(map(int,input().split()))for _ in range(n)]
+max_dist=-sys.maxsize
 min_dist=sys.maxsize
 ans=[]
 ans2=[]
+ans3=[]
 def choose2(nums,cnts):
-   
-    global min_dist
+    global max_dist
     if nums==3:
         
         x1,y1=L[ans[ans2[0]]]
         x2,y2=L[ans[ans2[1]]]
+        #print(x1,y1)
+        #print(x2,y2)
         dist=(((x1-x2)**2)+((y1-y2)**2))
-        min_dist=min(min_dist,dist)
+        max_dist=max(max_dist,dist)
+                
+        
+        
 
-        return
+        return 
     for i in range(cnts,len(ans)):
         if i not in ans2:
             ans2.append(i)
             choose2(nums+1,i)
             ans2.pop()
-    return
+    return 
     
     
     
 
 
 def choose(num,cnt):
-    
+    global min_dist
+    global max_dist
     if num==m+1:
         
         choose2(1,0)
-                
+        
+        min_dist=min(min_dist,max_dist)
+        max_dist=-sys.maxsize
         return
     for i in range(cnt,n):
         if i not in ans:
@@ -40,4 +49,4 @@ def choose(num,cnt):
             ans.pop()
     return
 choose(1,0)
-print(int(min_dist))
+print((min_dist))
