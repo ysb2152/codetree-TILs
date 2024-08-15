ab9f148360125a0b7738a163a6d14e2c9abc2858
ysb2152 <ysb2152@gmail.com>
2024-08-16 03:55:07 +0900

    Title: 비를 피하기, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240816/비를 피하기/stay-out-of-rain.py
+++ 240816/비를 피하기/stay-out-of-rain.py
@@ -1,11 +1,12 @@
 from collections import deque
-q=deque()
+
 n,h,m=map(int,input().split())
 visited=[[0 for _ in range(n)]for _ in range(n)]
 step=[[0 for _ in range(n)]for _ in range(n)]
 grid=[list(map(int,input().split()))for _ in range(n)]
 safearea=[]
 people=[]
+q=deque()
 for i in range(n):
     for j in range(n):
         if grid[i][j]==2:
@@ -16,37 +17,37 @@ def in_range(a,b):
     return 0<=a<n and 0<=b<n
 def push(a,b,s):
     visited[a][b]=1
-    step[a][b]=s
-    q.append((a,b))
+    q.append((a,b,s))
 def can_move(a,b):
     if not in_range(a,b):
         return False
     if visited[a][b] or grid[a][b]==1:
         return False
     return True
-def bfs():
+def bfs(s):
+    
     dxs,dys=[-1,1,0,0],[0,0,-1,1]
     while q:
-        a,b=q.popleft()
+        a,b,s=q.popleft()
         for dx,dy in zip(dxs,dys):
             new_a,new_b=a+dx,b+dy
             if can_move(new_a,new_b):
-                visited[new_a][new_b]
-                push(new_a,new_b,step[a][b]+1)
+                
+                if (new_a,new_b) in safearea:
+                    return s
+                
+                visited[new_a][new_b]=1
+                push(new_a,new_b,s+1)
+                
 new_grid=[[0 for _ in range(n)]for _ in range(n)]
 
 for k in range(h):
     a,b=people[k]
     visited[a][b]=1
-    push(a,b,0)
-    bfs()
-    min_move=99999
-    for p in range(len(safearea)):
-        x1,y1=safearea[p]
-        if step[x1][y1]!=0:
-            min_move=min(min_move,step[x1][y1])
-    new_grid[a][b]=min_move
-    if (a,b) in people and new_grid[a][b]==99999:
+    push(a,b,1)
+    s=bfs(1)
+    new_grid[a][b]=s
+    if new_grid[a][b]==None:
         new_grid[a][b]=-1
     visited=[[0 for _ in range(n)]for _ in range(n)]
     step=[[0 for _ in range(n)]for _ in range(n)]
