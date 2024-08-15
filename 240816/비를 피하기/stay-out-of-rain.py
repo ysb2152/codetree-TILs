a8582adabe487c344a7a1ce725ba7aa3f9df6584
ysb2152 <ysb2152@gmail.com>
2024-08-16 04:18:11 +0900

    Title: 비를 피하기, Time: 5013ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 240816/비를 피하기/stay-out-of-rain.py
+++ 240816/비를 피하기/stay-out-of-rain.py
@@ -2,17 +2,12 @@ from collections import deque
 
 n,h,m=map(int,input().split())
 visited=[[0 for _ in range(n)]for _ in range(n)]
-step=[[0 for _ in range(n)]for _ in range(n)]
+
 grid=[list(map(int,input().split()))for _ in range(n)]
-safearea=[]
-people=[]
+
+
 q=deque()
-for i in range(n):
-    for j in range(n):
-        if grid[i][j]==2:
-            people.append((i,j))
-        if grid[i][j]==3:
-            safearea.append((i,j))
+
 def in_range(a,b):
     return 0<=a<n and 0<=b<n
 def push(a,b,s):
@@ -33,7 +28,7 @@ def bfs(s):
             new_a,new_b=a+dx,b+dy
             if can_move(new_a,new_b):
                 
-                if (new_a,new_b) in safearea:
+                if grid[new_a][new_b]==3:
                     minimum.append(s)
                 
                 visited[new_a][new_b]=1
@@ -44,19 +39,15 @@ def bfs(s):
         return min(minimum)
                 
 new_grid=[[0 for _ in range(n)]for _ in range(n)]
-
-for k in range(h):
-    a,b=people[k]
-    visited[a][b]=1
-    push(a,b,1)
-    s=bfs(1)
-    new_grid[a][b]=s
-    if new_grid[a][b]==None:
-        new_grid[a][b]=-1
-    visited=[[0 for _ in range(n)]for _ in range(n)]
-    step=[[0 for _ in range(n)]for _ in range(n)]
-
-
+for i in range(n):
+    for j in range(n):
+        if grid[i][j]==2:
+            visited[i][j]=1
+            push(i,j,1)
+            new_grid[i][j]=bfs(1)
+            if new_grid[i][j]==None:
+                new_grid[i][j]=-1
+            visited=[[0 for _ in range(n)]for _ in range(n)]
     
 for row in new_grid:
     for ele in row:
