2deeb16bbdc2d925056a61c90ff1ebad09af14f8
ysb2152 <ysb2152@gmail.com>
2024-08-19 03:26:05 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 400ms, Memory: 34MB, Status: Passed - CodeTree

--- 240819/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240819/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -5,18 +5,18 @@ grid=[list(map(int,input().split()))for _ in range(n)]
 dp=[[0 for _ in range(n)]for _ in range(n)]
 def in_range(a,b):
     return 0<=a<n and 0<=b<n
-cnt=1
+
 def can_move(a,b):
-    global max_cnt
-    global cnt
+    if dp[a][b]!=0:
+        return dp[a][b]
+    cnt=1
     dxs,dys=[-1,1,0,0],[0,0,-1,1]
     for dx,dy in zip(dxs,dys):
         new_a,new_b=a+dx,b+dy
         if in_range(new_a,new_b) and grid[new_a][new_b]>grid[a][b]:
-            cnt+=1
-            can_move(new_a,new_b)
-            max_cnt=max(max_cnt,cnt)
-            cnt=1
+            cnt=max(cnt,can_move(new_a,new_b)+1)
+    dp[a][b]=cnt
+    return cnt
             
     
 
@@ -24,7 +24,7 @@ def can_move(a,b):
     return 0
 for i in range(n):
     for j in range(n):
-        can_move(i,j)
+        max_cnt=max(max_cnt,can_move(i,j))
 #can_move(1,1)
 if max_cnt!=-sys.maxsize:
     print(max_cnt)
