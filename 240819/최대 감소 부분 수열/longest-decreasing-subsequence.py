d18b1e297451060aa516227303dc8ac4388b49f2
ysb2152 <ysb2152@gmail.com>
2024-08-19 05:09:12 +0900

    Title: 최대 감소 부분 수열, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240819/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 240819/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -1,9 +1,11 @@
 n=int(input())
 L=[0]+list(map(int,input().split()))
-dp=[0 for _ in range(n+1)]
+dp=[1 for _ in range(n+1)]
 dp[1]=1
 for i in range(1,n+1):
     for j in range(i):
+        if i==n and L[j]>L[i]:
+            dp[i]=dp[j]+1
         if L[j]>L[i]:
             dp[i]=max(dp[i],dp[j]+1)
 print(max(dp))
