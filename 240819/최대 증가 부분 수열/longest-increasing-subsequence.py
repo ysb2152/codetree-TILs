d6228b6bf37f8b65a7f9145bb74d224fa40539a8
ysb2152 <ysb2152@gmail.com>
2024-08-19 05:01:46 +0900

    Title: 최대 증가 부분 수열, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240819/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,12 @@
+import sys
+n=int(input())
+L=[0]+list(map(int,input().split()))
+dp=[0 for _ in range(n+1)]
+
+
+dp[1]=1
+for i in range(1,n+1):
+    for j in range(i):
+        if L[j]<L[i]:
+            dp[i]=max(dp[i],dp[j]+1)
+print(max(dp))
