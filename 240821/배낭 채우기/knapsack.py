6aef86b9779c99c4cf2258beababa9a9d87eb80b
ysb2152 <ysb2152@gmail.com>
2024-08-21 23:54:54 +0900

    Title: 배낭 채우기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240821/배낭 채우기/knapsack.py
@@ -0,0 +1,11 @@
+import sys
+n,m=map(int,input().split())
+jewel=[tuple(map(int,input().split()))for _ in range(n)]
+jewel=[0]+jewel
+dp=[-sys.maxsize]*(m+1)
+dp[0]=0
+for i in range(1,n+1):
+    for j in range (m,-1,-1):
+        if j>=jewel[i][0]:
+            dp[j]=max(dp[j],dp[j-jewel[i][0]]+jewel[i][1])
+print(dp[m])
