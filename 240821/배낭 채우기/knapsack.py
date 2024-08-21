24f9dbcf6edb95224eb1beafa4777ac864ef7d15
ysb2152 <ysb2152@gmail.com>
2024-08-22 00:00:00 +0900

    Title: 배낭 채우기, Time: 209ms, Memory: 25MB, Status: Passed - CodeTree

--- 240821/배낭 채우기/knapsack.py
+++ 240821/배낭 채우기/knapsack.py
@@ -2,7 +2,7 @@ import sys
 n,m=map(int,input().split())
 jewel=[tuple(map(int,input().split()))for _ in range(n)]
 jewel=[0]+jewel
-dp=[-sys.maxsize]*(m+1)
+dp=[0]*(m+1)
 dp[0]=0
 for i in range(1,n+1):
     for j in range (m,-1,-1):
