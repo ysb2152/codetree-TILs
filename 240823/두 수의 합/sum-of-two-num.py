596edaacf7c5a9e8d854e85dfd4a68ef9bd6c521
ysb2152 <ysb2152@gmail.com>
2024-08-23 02:02:29 +0900

    Title: 두 수의 합, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240823/두 수의 합/sum-of-two-num.py
+++ 240823/두 수의 합/sum-of-two-num.py
@@ -2,9 +2,17 @@ n,k=map(int,input().split())
 hashmap={}
 nums=list(map(int,input().split()))
 for i in range(n):
-    hashmap[nums[i]]=1
+    hashmap[nums[i]]=0
+for i in range(n):
+    hashmap[nums[i]]+=1
 cnt=0
 for ele in hashmap:
     if k-ele in hashmap:
-        cnt+=1
+        if hashmap[ele]>=2:
+            cnt+=(hashmap[ele]*(hashmap[ele]-1))//2
+        else:
+            cnt+=1
+
+
+        
 print(cnt//2)
