4b7dff0b3f2614ea18412b72998017ef24c34cc3
ysb2152 <ysb2152@gmail.com>
2024-08-23 01:45:18 +0900

    Title: 가장 많은 데이터, Time: 266ms, Memory: 35MB, Status: Passed - CodeTree

--- 240823/가장 많은 데이터/most-frequent-data.py
+++ 240823/가장 많은 데이터/most-frequent-data.py
@@ -1,16 +1,15 @@
 n=int(input())
 hashmap={}
-lists=[]
+
 max_enc=-9999
 for i in range(n):
     chrs=input()
-    if chrs not in lists:
-        lists.append(chrs)
+    
     if chrs not in hashmap:
         hashmap[chrs]=0
     if chrs in hashmap:
         hashmap[chrs]+=1
-for ele in lists:
+for ele in hashmap:
     max_enc=max(max_enc,hashmap[ele])
 
 print(max_enc)
