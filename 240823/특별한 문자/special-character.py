c581a0e08ad94898a41ee8f169e1ad1ca33fe59e
ysb2152 <ysb2152@gmail.com>
2024-08-23 03:50:29 +0900

    Title: 특별한 문자, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240823/특별한 문자/special-character.py
+++ 240823/특별한 문자/special-character.py
@@ -1,6 +1,7 @@
 chrs=input()
 chrs=list(chrs)
 dic={}
+cnt=0
 for i in range(len(chrs)):
     if chrs[i] not in dic:
         dic[chrs[i]]=1
@@ -10,3 +11,6 @@ for ele in dic:
     if dic[ele]==1:
         print(ele)
         break
+    cnt=1
+if cnt==1:
+    print("None")
