be0e2f59f7170bd06e892f2eef553fde83e8eca4
ysb2152 <ysb2152@gmail.com>
2024-08-24 01:54:12 +0900

    Title: 대칭 차집합, Time: 5027ms, Memory: 37MB, Status: Time Limit Exceed - CodeTree

--- 240824/대칭 차집합/symmetric-difference-set.py
+++ 240824/대칭 차집합/symmetric-difference-set.py
@@ -5,11 +5,8 @@ b=list(map(int,input().split()))
 new=[]
 new=set(new)
 for ele in b:
-    if ele not in a and ele not in new:
+    if ele in a:
         new.add(ele)
 
-for ele in a:
-    if ele not in b and ele not in new:
-        new.add(ele)
 
-print(len(new))
+print(n+m-(2*len(new)))
