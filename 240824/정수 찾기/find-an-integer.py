d46d2ee9e2acc65612336009c91159c8c8503f3f
ysb2152 <ysb2152@gmail.com>
2024-08-24 00:16:04 +0900

    Title: 정수 찾기, Time: 5059ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

---
+++ 240824/정수 찾기/find-an-integer.py
@@ -0,0 +1,9 @@
+n=int(input())
+a=list(map(int,input().split()))
+m=int(input())
+b=list(map(int,input().split()))
+for ele in b:
+    if ele not in a:
+        print("0")
+    else:
+        print("1")
