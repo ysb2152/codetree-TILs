b9ee96541db788d3ba8276654e83960357235121
ysb2152 <ysb2152@gmail.com>
2024-08-26 02:15:23 +0900

    Title: treeset 기본, Time: 1060ms, Memory: 31MB, Status: Passed - CodeTree

--- 240826/treeset 기본/treeset-basic.py
+++ 240826/treeset 기본/treeset-basic.py
@@ -5,38 +5,56 @@ for _ in range(n):
     a=input()
     if a.startswith("add"):
         _,x=a.split()
+        x=int(x)
         if x in ss:
             continue
         else:
             ss.add(x)
-    elif a.startswith("remove"):
+            
+            continue
+    if a.startswith("remove"):
         _,x=a.split()
+        x=int(x)
         ss.remove(x)
-    elif a.startswith("find"):
+        continue
+    if a.startswith("find"):
         _,x=a.split()
+        x=int(x)
         if x in ss:
             print("true")
+            continue
         else:
             print("false")
-    elif a.startswith("lower_bound"):
+            continue
+    if a.startswith("lower_bound"):
         _,x=a.split()
+        x=int(x)
         if ss.bisect_left(x)==len(ss):
             print("None")
+            continue
         else:
             print(ss[ss.bisect_left(x)])
-    elif a.startswith("upper_bound"):
+            continue
+    if a.startswith("upper_bound"):
         _,x=a.split()
+        x=int(x)
         if ss.bisect_right(x)==len(ss):
             print("None")
+            continue
         else:
             print(ss[ss.bisect_right(x)])
-    elif a.startswith("largest"):
+            continue
+    if a.startswith("largest"):
         if len(ss)==0:
             print("None")
+            continue
         else:
             print(ss[-1])
-    elif a.startswith("smallest"):
+            continue
+    if a.startswith("smallest"):
         if len(ss)==0:
             print("None")
+            continue
         else:
             print(ss[0])
+            continue
