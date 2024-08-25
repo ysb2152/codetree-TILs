3e9cd5a719278200f4cd75f37f4de25bba690d3b
ysb2152 <ysb2152@gmail.com>
2024-08-26 05:39:43 +0900

    Title: 점 빼기, Time: 2043ms, Memory: 49MB, Status: Wrong Answer - CodeTree

---
+++ 240826/점 빼기/remove-point.py
@@ -0,0 +1,18 @@
+import sys
+from sortedcontainers import SortedSet
+n,m=map(int,input().split())
+L=[]
+for _ in range(n):
+    a=tuple(map(int,input().split()))
+    L.append(a)
+ss=SortedSet(L)
+
+for _ in range(m):
+    k=int(input())
+    k=(k,sys.maxsize)
+    index=ss.bisect_left(k)
+    if index==len(ss):
+        print("-1 -1")
+    else:
+        print(ss[index][0],ss[index][1])
+        ss.remove(ss[index])
