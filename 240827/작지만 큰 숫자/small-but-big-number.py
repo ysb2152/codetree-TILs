44cef1207b8ea319d785aae65de2dba112ddb68d
ysb2152 <ysb2152@gmail.com>
2024-08-27 03:10:44 +0900

    Title: 작지만 큰 숫자, Time: 1490ms, Memory: 41MB, Status: Wrong Answer - CodeTree

--- 240827/작지만 큰 숫자/small-but-big-number.py
+++ 240827/작지만 큰 숫자/small-but-big-number.py
@@ -6,9 +6,12 @@ ss=SortedSet(arr)
 nums=list(map(int,input().split()))
 for i in range(m):
     num=nums[i]
-    if ss.bisect_right(num)==len(ss) or ss.bisect_right(num)==0:
+    if ss.bisect_left(num)==len(ss) or ss.bisect_right(num)==0:
         print("-1")
         continue
+    elif num in ss:
+        print(num)
+        ss.remove(num)
     else:
-        print(ss[ss.bisect_right(num)-1])
-        ss.remove(ss[ss.bisect_right(num)-1])
+        print(ss[ss.bisect_left(num)-1])
+        ss.remove(ss[ss.bisect_left(num)-1])
