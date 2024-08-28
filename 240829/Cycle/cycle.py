8e6f370d74d464f11a7f0786e32aedb0e6e719e4
ysb2152 <ysb2152@gmail.com>
2024-08-29 00:17:32 +0900

    Title: Cycle, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240829/Cycle/cycle.py
+++ 240829/Cycle/cycle.py
@@ -1,13 +1,16 @@
 n,p=map(int,input().split())
 ans=[]
+
 a=(n*n)%p
 ans.append(a)
 while True:
     new_a=(a*n)%p
     
     if new_a in ans:
+        num=new_a
         break
     ans.append(new_a)
     
     a=new_a
-print(len(ans))
+#print(num)
+print(len(ans)-ans.index(num))
