0fc74723939d8556e19a472b1fca6f0a70818091
ysb2152 <ysb2152@gmail.com>
2024-08-29 03:20:02 +0900

    Title: 가장 가까운 점, Time: 5017ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

---
+++ 240829/가장 가까운 점/nearest-point.py
@@ -0,0 +1,23 @@
+import heapq
+n,m=map(int,input().split())
+arr=[tuple(map(int,input().split()))for _ in range(n)]
+hq=[]
+for point in arr:
+    a,b=point
+    heapq.heappush(hq,(abs(a)+abs(b),a,b))
+    heapq.heapify(hq)
+    
+for _ in range(m):
+    newhq=[]
+    for point in hq:
+        _,a,b=point
+        heapq.heappush(newhq,(abs(a)+abs(b),a,b))
+        heapq.heapify(newhq)
+    newhq[0]=list(newhq[0])
+    newhq[0][1]+=2
+    newhq[0][2]+=2
+    newhq[0][0]=abs(newhq[0][1])+abs(newhq[0][2])
+    newhq[0]=tuple(newhq[0])
+    heapq.heapify(newhq)
+    hq=newhq
+print(hq[0][1],hq[0][2])
