27a738c30fa051a257aff6bb4667f4b292f436ae
ysb2152 <ysb2152@gmail.com>
2024-08-29 05:13:05 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5088ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 240829/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240829/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -3,18 +3,21 @@ import sys
 
 n=int(input())
 max_avg=-sys.maxsize
+pq=[]
 arr=list(map(int,input().split()))
-for k in range(1,n-1):
-    pq=[]
-    
-    for i in range(k,n):
-        heapq.heappush(pq,arr[i])
+for i in range(n-2,n):
+    heapq.heappush(pq,arr[i])
+#print(pq)
+for k in range(n-3,-1,-1):
+    heapq.heappush(pq,arr[k])
+    #print(pq)
     heapq.heapify(pq)
     #print(pq)
-    #print(-sum(pq))
-    heapq.heappop(pq)
+    
+    a=heapq.heappop(pq)
     #print(pq)
     #print(sum(pq)/(n-k-1))
-    max_avg=max(max_avg,sum(pq)/(n-k-1))
+    max_avg=max(max_avg,sum(pq)/len(pq))
+    heapq.heappush(pq,a)
     
 print(f"{max_avg:.2f}")
