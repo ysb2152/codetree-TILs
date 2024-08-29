5f58d605d598bd1ebf69b27b6458620bbda16e04
ysb2152 <ysb2152@gmail.com>
2024-08-29 21:46:46 +0900

    Title: 최솟값 3개, Time: 546ms, Memory: 32MB, Status: Passed - CodeTree

--- 240829/최솟값 3개/top-3-smallest-number.py
+++ 240829/최솟값 3개/top-3-smallest-number.py
@@ -4,7 +4,15 @@ arr=list(map(int,input().split()))
 pq=[]
 for i in range(n):
     heapq.heappush(pq,arr[i])
+        
     if len(pq)<3:
         print("-1")
     else:
-        print(pq[0]*pq[1]*pq[2])
+        a=heapq.heappop(pq)
+        b=heapq.heappop(pq)
+        c=heapq.heappop(pq)
+        print(a*b*c)
+        heapq.heappush(pq,a)
+        heapq.heappush(pq,b)
+        heapq.heappush(pq,c)
+    #print(pq)
